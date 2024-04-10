bdfeba2756f56da596ca8c33b2410f8e157b04f7
Valeriy <valeriy.elykomov@gmail.com>
2024-04-10 14:18:23 +0500

    Create API tests project

---
+++ GostCryptographyAPITests/Controllers/CMSControllerTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using GostCryptographyAPI.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GostCryptographyAPI.Controllers.Tests
+{
+    [TestClass()]
+    public class CMSControllerTests
+    {
+        [TestMethod()]
+        public void SignMessageTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
