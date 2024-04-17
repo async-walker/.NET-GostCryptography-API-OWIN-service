931fdfe719e15b90c21132c21741f76d59f088b4
Valeriy <valeriy.elykomov@gmail.com>
2024-04-17 12:38:17 +0500

    Create ResponseError.cs

---
+++ GostCryptographyAPI/Exceptions/ResponseError.cs
@@ -0,0 +1,16 @@
+﻿namespace GostCryptographyAPI.Exceptions
+{
+    internal class ResponseError
+    {
+        public ResponseError(int statusCode, string code, string message)
+        {
+            StatusCode = statusCode;
+            Code = code;
+            Message = message;
+        }
+
+        public int StatusCode { get; private set; }
+        public string Code { get; private set; }
+        public string Message { get; private set; }
+    }
+}
