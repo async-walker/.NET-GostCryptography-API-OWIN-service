74fda24c71cb0a19fadad543c3b320450a31b0f5
Valeriy <valeriy.elykomov@gmail.com>
2024-04-17 12:38:12 +0500

    Create ResponseCodes.cs

---
+++ GostCryptographyAPI/Types/ResponseCodes.cs
@@ -0,0 +1,9 @@
+﻿namespace GostCryptographyAPI.Types
+{
+    internal class ResponseCodes
+    {
+        public const string CERT_NOT_FOUND = "CERT_NOT_FOUND";
+        public const string CRYPTOGRAPHYC_EXCEPTION = "CRYPTOGRAPHYC_EXCEPTION";
+        public const string OTHER_EXCEPTION = "OTHER_EXCEPTION";
+    }
+}
