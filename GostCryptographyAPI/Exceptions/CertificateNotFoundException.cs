1d30a9fc91bbae0331ec824a8d0c81e0be071a4b
Valeriy <valeriy.elykomov@gmail.com>
2024-04-17 12:38:09 +0500

    Update CertificateNotFoundException.cs

--- GostCryptographyAPI/Exceptions/CertificateNotFoundException.cs
+++ GostCryptographyAPI/Exceptions/CertificateNotFoundException.cs
@@ -1,25 +1,40 @@
 ﻿using System;
-using System.Runtime.Serialization;
+using System.Security.Cryptography.X509Certificates;
 
 namespace GostCryptographyAPI.Exceptions
 {
     [Serializable]
     internal class CertificateNotFoundException : Exception
     {
-        public CertificateNotFoundException()
+        public CertificateNotFoundException(
+            StoreLocation storeLocation,
+            StoreName storeName,
+            X509FindType findType,
+            string findValue) 
+            : base(
+                GetMessageEx(storeLocation, storeName, findType, findValue))
         {
+            StoreLocation = storeLocation;
+            StoreName = storeName;
+            FindType = findType;
         }
 
-        public CertificateNotFoundException(string message) : base(message)
-        {
-        }
+        public StoreLocation StoreLocation { get; set; }
+        public StoreName StoreName { get; set; }
+        public X509FindType FindType { get; set; }
+        public string FindValue { get; set; }
 
-        public CertificateNotFoundException(string message, Exception innerException) : base(message, innerException)
+        private static string GetMessageEx(
+            StoreLocation storeLocation,
+            StoreName storeName,
+            X509FindType findType,
+            string findValue)
         {
-        }
+            var message =
+                $"Сертификат с поиском по {findType} и значением [{findValue}] " +
+                $"не был найден в хранилище {storeLocation}/{storeName}";
 
-        protected CertificateNotFoundException(SerializationInfo info, StreamingContext context) : base(info, context)
-        {
+            return message;
         }
     }
 }
